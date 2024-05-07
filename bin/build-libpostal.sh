7cd6dc0d9dbcc6fac92d390a0d39dedfb1ffbd14
Nick Crews <nicholas.b.crews@gmail.com>
2024-05-07 11:45:49 -0800

    update libpostal build script

--- bin/build-libpostal.sh
+++ bin/build-libpostal.sh
@@ -5,6 +5,7 @@ cd libpostal
 # Per emscripten docs, we need to use emconfigure and emmake instead of configure and make
 # to build the lib to wasm
 # https://emscripten.org/docs/compiling/Building-Projects.html?highlight=link#integrating-with-a-build-system
-emconfigure ./configure --datadir="${DATA_DIR}" --disable-sse2 MODEL=senzing
+# the LT_MULTI_MODULE=1 flag is there to avoid a `-force_load` error:
+# https://github.com/emscripten-core/emscripten/issues/11764#issuecomment-679456112
+LT_MULTI_MODULE=1 emconfigure ./configure --datadir="${DATA_DIR}" --disable-sse2 MODEL=senzing
 emmake make -j10
-sudo make install
